abb3c553c1f47066960b562e22384111aa5b1134
Carlos Santiago <carlos.san.rol@gmail.com>
2020-12-22 13:43:09 +0900

    day6 part2

--- 6/main.go
+++ 6/main.go
@@ -25,8 +25,8 @@ func main() {
 
     file.Close() 
 
-    fmt.Println(partOne(lines))
-    //fmt.Println(partTwo(lines))
+    //fmt.Println(partOne(lines))
+    fmt.Println(partTwo(lines))
 }
 
 func partOne(lines []string) int {
@@ -46,6 +46,7 @@ func partOne(lines []string) int {
             continue
         }
         for _, char := range line {
+            //fmt.Printf("%s - %b\n", string(char), char)
             if !strings.ContainsRune(groupAnswers.String(), char) {
                 groupAnswers.WriteRune(char)
             }
@@ -61,6 +62,33 @@ func partOne(lines []string) int {
 
 
 func partTwo(lines []string) int {
-    return 0
+    var sum int
+    var currentReferenceLine strings.Builder
+    currentReferenceLine.WriteString(lines[0])
+    for i := 1; i < len(lines); {
+        line := lines[i]
+        //fmt.Printf("%s - %b\n", string(line), line)
+
+        prevReferenceLine := currentReferenceLine.String()
+        currentReferenceLine.Reset()
+        for _, char := range prevReferenceLine {
+            if strings.ContainsRune(line, char) {
+                currentReferenceLine.WriteRune(char)
+            }
+        }
+
+        if i < len(lines)-1 && lines[i+1] == "" {
+            fmt.Println(currentReferenceLine.String())
+            sum += currentReferenceLine.Len()
+            currentReferenceLine.Reset()
+            currentReferenceLine.WriteString(lines[i+2])
+            i += 2
+            continue
+        }
+        i++
+    }
+    sum += currentReferenceLine.Len()
+
+    return sum
 }
 
