2758e31596f5e82583af7a459532bf2fe617cb58
Carlos Santiago <carlos.san.rol@gmail.com>
2020-12-23 15:23:25 +0900

    day7 part1

---
+++ 7/main.go
@@ -0,0 +1,83 @@
+package main 
+  
+import ( 
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "strings"
+) 
+
+type tree struct {
+    references map[string][]string
+    result []string
+}
+
+func main() { 
+    file, err := os.Open("input.txt") 
+  
+    if err != nil { 
+        log.Fatalf("failed to open") 
+    } 
+    scanner := bufio.NewScanner(file) 
+    scanner.Split(bufio.ScanLines) 
+    var lines []string
+
+    for scanner.Scan() {
+        lines = append(lines, scanner.Text())
+    }
+
+    file.Close() 
+
+    fmt.Println(partOne(lines))
+    //fmt.Println(partTwo(lines))
+}
+
+func partOne(lines []string) int {
+    t := tree {
+        references: make(map[string][]string),
+    }
+    
+    for _, line := range lines {
+        a := strings.Split(line, " bags contain ")
+        bag_color := strings.Replace(a[0], " ", "_", -1)
+        b := strings.Split(a[1], ", ")
+        for _, i := range b {
+            c := strings.Split(i, " bag")
+            in_bag_color := strings.Replace(c[0][2:], " ", "_", -1)
+            t.references[in_bag_color] = append(t.references[in_bag_color], bag_color)
+        }
+
+    }
+    t.iterateTree("shiny_gold")
+
+    return len(t.result)
+}
+
+
+
+func (t *tree) iterateTree(branch string) {
+    root := t.references[branch]
+    for _, leaf := range root {
+        if t.resultContains(leaf) {
+            continue
+        }
+        t.result = append(t.result, leaf)
+        t.iterateTree(leaf)
+    }
+}
+
+func (t *tree) resultContains(bagName string) bool {
+    for _, r := range t.result {
+        if r == bagName {
+            return true
+        }
+    }
+    return false
+}
+
+func partTwo(lines []string) int {
+
+    return 0
+}
+
